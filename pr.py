0c8a1fe0b6f1c3109d20bf063214855b04f45b64
Kamron <soliev.kamronbek@gmail.com>
2021-11-25 05:57:52 +0200

    Initial commit

---
+++ pr.py
@@ -0,0 +1,16 @@
+with open('requirements.txt') as f:
+    line = f.readlines()
+
+with open('ignore_req_packages.txt') as f2:
+    line2 = f2.readline()
+
+m = line2
+print(len(line))
+m = []
+
+for l in m:
+    for l2 in line2:
+        if l.strip() == l2.strip():
+            m.remove(l)
+
+print(len(m))
